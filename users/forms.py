829a0a9445c5710c7edef61ae18a1f23d974a277
dippersport <ceyhun071173@mail.ru>
2024-03-15 12:03:22 +0400

    All cart templates were included

--- users/forms.py
+++ users/forms.py
@@ -11,19 +11,19 @@ class UserLoginForm(AuthenticationForm):
             "username",
             "password",)
 
-    username = forms.CharField()
-    password = forms.CharField()
-    # username = forms.CharField(
-    #     label = 'Имя',
-    #     widget=forms.TextInput(attrs={"autofocus": True,
-    #                                   'class': 'form-control',
-    #                                   'placeholder':'Введите ваше имя пользователя'}))
-    # password = forms.CharField(
-    #     label = 'Пароль',
-    #     widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
-    #                                       'class': 'form-control',
-    #                                       'placeholder':'Введите ваш пароль'}),
-    # )
+    #username = forms.CharField()
+    #password = forms.CharField()
+    username = forms.CharField(
+        label = 'Имя',
+        widget=forms.TextInput(attrs={"autofocus": True,
+                                      'class': 'form-control',
+                                      'placeholder':'Введите ваше имя пользователя'}))
+    password = forms.CharField(
+        label = 'Пароль',
+       widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
+                                           'class': 'form-control',
+                                           'placeholder':'Введите ваш пароль'}),
+     )
 
     
 class UserRegistrationForm(UserCreationForm):
