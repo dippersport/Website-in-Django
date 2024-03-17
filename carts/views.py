c074a975e9503a1717a8a64af465e0d0c0e8344d
dippersport <ceyhun071173@mail.ru>
2024-03-17 14:42:22 +0400

    Sessions - carts for anonymous user

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,5 @@
 from urllib import response
+from django.contrib import sessions
 from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
@@ -8,6 +9,7 @@ from carts.models import Cart
 
 def cart_add(request):
     product_id = request.POST.get("product_id")
+
     product = Products.objects.get(id=product_id)
 
     if request.user.is_authenticated:
@@ -20,6 +22,21 @@ def cart_add(request):
                 cart.save()
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
+
+    else:
+        carts = Cart.objects.filter(
+            session_key=request.session.session_key, product=product)
+        
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(
+                session_key=request.session.session_key, product=product, quantity=1)
+
+
     user_cart = get_user_carts(request)
     cart_items_html = render_to_string(
         "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib import auth, messages
 from django.http import  HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
-
+from carts.models import Cart
 from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
@@ -14,9 +14,15 @@ def login(request):
           username = request.POST['username']
           password = request.POST['password']
           user = auth.authenticate(username=username, password=password)
+
+          session_key = request.session.session_key
+
           if user:
               auth.login(request, user)
-              messages.success(request, f'(username), Вы вошли в аккаунт')
+              messages.success(request, f'{username}, Вы вошли в аккаунт')
+
+              if session_key:
+                  Cart.objects.filter(session_key=session_key).update(user=user)
               
               redirect_page = request.POST.get('next', None)
               if redirect_page and redirect_page != reverse('user:logout'):
@@ -36,9 +42,15 @@ def registration(request):
        form = UserRegistrationForm(data=request.POST)
        if form.is_valid():
           form.save()
+
+          session_key = request.session.session_key
+
           user = form.instance
           auth.login(request, user)
-          messages.success(request, f'(user.username), Вы успешно зарегистрировались и вошли в аккаунт')
+
+          if session_key:
+              Cart.objects.filter(session_key=session_key).update(user=user)
+          messages.success(request, f'{user.username}, Вы успешно зарегистрировались и вошли в аккаунт')
           return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserRegistrationForm()
@@ -71,6 +83,6 @@ def users_cart(request):
     return render(request, 'users/users_cart.html')
 
 def logout(request):
-    messages.success(request, f'(request.user.username), Вы вышли из аккаунта')
+    messages.success(request, f'{request.user.username}, Вы вышли из аккаунта')
     auth.logout(request)
     return redirect(reverse('main:index'))
